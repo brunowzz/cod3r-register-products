755b642464715921495dca5acbdb7642e4238805
Bruno Vinícius Barreiras de Oliveira <94939630+brunowzz@users.noreply.github.com>
2024-04-04 13:59:57 -0300

    feat: add product update

---
+++ src/app/components/template/product-update/product-update.component.ts
@@ -0,0 +1,41 @@
+import { Product } from './../../../interfaces/product.model';
+import { Component } from '@angular/core';
+import { ProductService } from '../../../services/product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-product-update',
+  templateUrl: './product-update.component.html',
+  styleUrl: './product-update.component.css',
+})
+export class ProductUpdateComponent {
+  product: Product = {
+    id: '',
+    name: '',
+    price: 0,
+  };
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.productService.readById(id!).subscribe((product) => {
+      this.product = product;
+    });
+  }
+
+  updateProduct(): void {
+    this.productService.updateProduct(this.product).subscribe(() => {
+      this.productService.showMessage('Produto atualizado com sucesso!');
+      this.router.navigate(['/products']);
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products']);
+  }
+}
