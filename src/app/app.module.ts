637dc5eeadd277db6d780c2588a485e39b7d51de
Bruno Vinícius Barreiras de Oliveira <94939630+brunowzz@users.noreply.github.com>
2024-04-04 11:33:23 -0300

    feat: add create product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,13 +10,18 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductCreateComponent } from './components/template/product-create/product-create.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-import { HomeComponent } from './views/home/home.component';
 import { MatCardModule } from '@angular/material/card';
-import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 import { RedDirective } from './directives/red.directive';
 
 @NgModule({
@@ -26,6 +33,7 @@ import { RedDirective } from './directives/red.directive';
     HomeComponent,
     ProductCrudComponent,
     RedDirective,
+    ProductCreateComponent,
   ],
   imports: [
     BrowserModule,
@@ -34,6 +42,11 @@ import { RedDirective } from './directives/red.directive';
     MatSidenavModule,
     MatListModule,
     MatCardModule,
+    MatButtonModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
---
+++ src/app/components/template/product-create/product-create.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { ProductService } from '../../../services/product.service';
+import { Router } from '@angular/router';
+import { Product } from '../../../interfaces/product.model';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrl: './product-create.component.css',
+})
+export class ProductCreateComponent {
+  product: Product = {
+    name: 'Produto',
+    price: 25.99,
+  };
+
+  constructor(private productService: ProductService, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto adicionado com sucesso!');
+      this.router.navigate(['/products']);
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products']);
+  }
+}
